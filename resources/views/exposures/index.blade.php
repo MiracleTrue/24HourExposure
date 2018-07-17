985c79f4cb4fc4ade054195e19538628bd2b96b0
Miracle <120007700@qq.com>
2018-07-17 08:26:07 +0800

    支付页面

--- resources/views/exposures/index.blade.php
+++ resources/views/exposures/index.blade.php
@@ -11,14 +11,14 @@
    {{dump($lbs)}}-->
 
     @include('common.error')
-    <!--<form action="{{route('payment.gift.alipay')}}" method="POST">
+   <form action="{{route('payment.gift.alipay')}}" method="POST">
         {{csrf_field()}}
 
         <input type="hidden" name="exposure_id" value="8">
-        <input type="hidden" name="gifts" value='[{"id":1,"number":1},{"id":7,"number":2}]'>
+        <input type="hidden" name="gifts" value='[{"id":1,"number":1},{"id":3,"number":2}]'>
 
         <input type="submit" value="OK">
-    </form>-->
+    </form>
 <header><span>曝光台</span> <a href="../add-exposure.php"></a></header>
 <div class="homebox">
 	<div class="location">
