f52fb154061ed943bc4e4ab8e753de538818de81
Miracle <120007700@qq.com>
2018-08-10 14:11:34 +0800

    微信支付5

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'payment/gift/alipay_notify'
+        'payment/gift/alipay_notify',
+        'payment/gift/wechat_notify'
     ];
 }
