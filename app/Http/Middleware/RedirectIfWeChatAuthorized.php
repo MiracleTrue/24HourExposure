5a2d0d29fe6c6976c4c72e961c7f2ec927132087
Miracle <120007700@qq.com>
2018-08-15 16:42:57 +0800

    openid  log

--- app/Http/Middleware/RedirectIfWeChatAuthorized.php
+++ app/Http/Middleware/RedirectIfWeChatAuthorized.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 /*如果未微信授权则重定向*/
 
@@ -18,29 +19,19 @@ class RedirectIfWeChatAuthorized
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if (!$request->session()->has('wechat_user'))
+        {
+            $app = app('wechat.official_account');
 
-        $app = app('wechat.official_account');
+            $request->session()->put('target_url', url()->previous());
 
-        $request->session()->put('target_url', url()->previous());
+            $response = $app->oauth->scopes(['snsapi_userinfo'])
+                ->redirect();
 
-        $response = $app->oauth->scopes(['snsapi_userinfo'])
-            ->redirect();
+            return $response;
+        }
 
-        return $response;
-
-
-//        if (!$request->session()->has('wechat_user'))
-//        {
-//            $app = app('wechat.official_account');
-//
-//            $request->session()->put('target_url', url()->previous());
-//
-//            $response = $app->oauth->scopes(['snsapi_userinfo'])
-//                ->redirect();
-//
-//            return $response;
-//        }
-
+        Log::error($request->session()->get('wechat_user'));
         return $next($request);
     }
 }
