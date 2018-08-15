6a4b6e85b65cb9ae126c5c4117eda5fffa0a2645
Miracle <120007700@qq.com>
2018-08-15 16:00:39 +0800

    前台微信判断

--- resources/views/exposures/show.blade.php
+++ resources/views/exposures/show.blade.php
@@ -2,86 +2,92 @@
 
 @section('title',$exposure->title)
 <style>
-	.pay_method {
-		width: 90%;
-		font-size: 0.13rem;
-		color: #333;
-		background: #EEEEEE;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		padding: 10px 5px;
-		border-radius: 25px;
-		 transform: translate(-50%,-50%);
-	}
- .pay_method label {
-    height: 1rem;
-    border-bottom: 1px solid #f2f2f2; 
-    line-height: 0.7rem;
-    display: flex;
-		align-items: center;
-		position: relative;
-}
- .pay_method label span{
-	 margin-left: 0.3rem;
-	 font-size: 0.25rem;
- }
- .pay_method label img:nth-of-type(1) {
-    width: 0.6rem;
-    height: 0.6rem;
-    display: block;
-		margin-left: 0.2rem;
-}
-input[type="radio"]{
- position: absolute;
- right: 10px;
- width: 0.3rem;
- height: 0.3rem;
-}
-input[type="button"]{
-	
-	    width: 1.5rem;
-    line-height: 0.5rem;
-    height: 0.5rem;
-    background: #d71f21;
-    color: #FFFFFF;
-    text-align: center;
-    border: none;
-    border-radius: 20px;
-		display: block;
-		margin-left: 20%;
-}
+    .pay_method {
+        width: 90%;
+        font-size: 0.13rem;
+        color: #333;
+        background: #EEEEEE;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        padding: 10px 5px;
+        border-radius: 25px;
+        transform: translate(-50%, -50%);
+    }
 
-/* input[type="checkbox"],
-input[type="radio"] {
-	display: none;
-} */
- 
-input[type="radio"]+ i {
-	border-radius: 7px;
-}
- 
-input[type="checkbox"]:checked+ i,
-input[type="radio"]:checked+ i {
-	background: #2489c5;
-}
+    .pay_method label {
+        height: 1rem;
+        border-bottom: 1px solid #f2f2f2;
+        line-height: 0.7rem;
+        display: flex;
+        align-items: center;
+        position: relative;
+    }
 
-.pay_confirm{
-	display: flex;
-	align-content: center;
-	justify-content: center;
-}
-	.pay_confirm div{
-		width: 1.5rem;
-		line-height: 0.5rem;
-		height: 0.5rem;
-		background: #00aa6d;
-		color: #FFFFFF;
-		text-align: center;
-		border: none;
-		border-radius: 20px;
-		display: block;
-	}
+    .pay_method label span {
+        margin-left: 0.3rem;
+        font-size: 0.25rem;
+    }
+
+    .pay_method label img:nth-of-type(1) {
+        width: 0.6rem;
+        height: 0.6rem;
+        display: block;
+        margin-left: 0.2rem;
+    }
+
+    input[type="radio"] {
+        position: absolute;
+        right: 10px;
+        width: 0.3rem;
+        height: 0.3rem;
+    }
+
+    input[type="button"] {
+
+        width: 1.5rem;
+        line-height: 0.5rem;
+        height: 0.5rem;
+        background: #d71f21;
+        color: #FFFFFF;
+        text-align: center;
+        border: none;
+        border-radius: 20px;
+        display: block;
+        margin-left: 20%;
+    }
+
+    /* input[type="checkbox"],
+    input[type="radio"] {
+        display: none;
+    } */
+
+    input[type="radio"] + i {
+        border-radius: 7px;
+    }
+
+    input[type="checkbox"]:checked + i,
+    input[type="radio"]:checked + i {
+        background: #2489c5;
+    }
+
+    .pay_confirm {
+        display: flex;
+        align-content: center;
+        justify-content: center;
+    }
+
+    .pay_confirm div {
+        width: 1.5rem;
+        line-height: 0.5rem;
+        height: 0.5rem;
+        background: #00aa6d;
+        color: #FFFFFF;
+        text-align: center;
+        border: none;
+        border-radius: 20px;
+        display: block;
+    }
 </style>
 @section('content')
     <form class="payform" action="{{route('payment.gift.alipay')}}" method="GET">
@@ -100,23 +106,20 @@ input[type="radio"]:checked+ i {
             <a href="{{route('root')}}"><</a>
             <span>曝光详情</span>
         </div>
-		
-		<!-- 支付方式选择 -->
-	<div class="pay_method">
-		<label style=""><img src="{{asset('web/img/alipay.png')}}"><span>支付宝支付</span> <input type="radio" name="pay_method" checked="checked" value="alipay" /></label>
-		<label style=""><img src="{{asset('web/img/wechat.png')}}"><span>微信支付</span> <input type="radio" name="pay_method"  value="wechat" /></label>
-		<div class="pay_confirm">
-			<div class="pay_cancel">取消</div>
-			<input class="pay_submit" type="button" value="立即支付" />
-		</div>
-		
-	</div>
-		
-		
-		
-		
-		
-		
+
+        <!-- 支付方式选择 -->
+        <div class="pay_method">
+            <label style=""><img src="{{asset('web/img/alipay.png')}}"><span>支付宝支付</span> <input type="radio" name="pay_method" checked="checked"
+                                                                                                 value="alipay"/></label>
+            <label style=""><img src="{{asset('web/img/wechat.png')}}"><span>微信支付</span> <input type="radio" name="pay_method" value="wechat"/></label>
+            <div class="pay_confirm">
+                <div class="pay_cancel">取消</div>
+                <input class="pay_submit" type="button" value="立即支付"/>
+            </div>
+
+        </div>
+
+
         <div class="comment">
             <span>{{$exposure->category->name}}</span>
             @foreach($exposure->gifts as $gift)
@@ -173,169 +176,69 @@ input[type="radio"]:checked+ i {
     </div>
     <script type="text/javascript" src="{{asset('web/library/jqueryJson/jquery.json.js')}}"></script>
     <script>
-		$(".pay_method").hide();
-		$(".pay_cancel").click(function(){
-			$(".pay_method").hide();
-		})
-		/* 判断是否为微信 */
-		/* function isWeiXin(){
-			var ua = window.navigator.userAgent.toLowerCase();
-			if(ua.match(/MicroMessenger/i) == 'micromessenger'){
-				$(".pay_method").find('p').eq(0).hide();
-				return true;
-			}else{
-				$(".pay_method").find('p').eq(1).hide();
-				return false;
-			}
-		} */
-		// isWeiXin();
-		
-		
-		
-		 $(".pay").each(function (i, index) {
-		            $(index).click(function () {
-					$(".pay_method").show();
-					
-					$(".pay_submit").on("click",function(){
-					 var pay=$('input:radio[name="pay_method"]:checked').val();
-					 /* alert(pay); */
-					 if(pay=="alipay"){
-						 				
-						var myJson = new Array();
-						var obj = new Object();
-	 
-						obj.id = $(index).attr('data-id');
-	 
-						obj.number = 1;
-	 
-	 
-						myJson.push(obj)
-	 
-	 
-						var strjson = $.toJSON(myJson);
-						console.log(strjson)
-						$("input[name='gifts']").val(strjson);
-	 
-						$('.payform').attr('action', '{{route("payment.gift.alipay")}}');
-						$('.payform').submit();
-						 				
-					 }else{
-						 				
-						var myJson = new Array();
-						var obj = new Object();
-	 
-						obj.id = $(index).attr('data-id');
-	 
-						obj.number = 1;
-	 
-	 
-						myJson.push(obj)
-	 
-	 
-						var strjson = $.toJSON(myJson);
-						console.log(strjson)
-						$("input[name='gifts']").val(strjson);
-	 
-						$('.payform').attr('action', '{{route("payment.gift.wechat")}}');
-						$('.payform').submit();
-						 				
-					 }
-					 
-					 
-					 
-					})
-					
-					
-					
-		           /*     if (i == '支付宝') {
-		                    var myJson = new Array();
-		                    var obj = new Object();
-		
-		                    obj.id = $(index).attr('data-id');
-		
-		                    obj.number = 1;
-		
-		
-		                    myJson.push(obj)
-		
-		
-		                    var strjson = $.toJSON(myJson);
-		                    console.log(strjson)
-		                    $("input[name='gifts']").val(strjson);
-		
-		                    $('.payform').attr('action', '{{route("payment.gift.alipay")}}');
-		                    $('.payform').submit();
-		                }
-		                else {
-		                    var myJson = new Array();
-		                    var obj = new Object();
-		
-		                    obj.id = $(index).attr('data-id');
-		
-		                    obj.number = 1;
-		
-		
-		                    myJson.push(obj)
-		
-		
-		                    var strjson = $.toJSON(myJson);
-		                    console.log(strjson)
-		                    $("input[name='gifts']").val(strjson);
-		
-		                    $('.payform').attr('action', '{{route("payment.gift.wechat")}}');
-		                    $('.payform').submit();
-		                } */
-		
-		            })
-		        })
-		
-		
-		
-      /*  $(".pay").each(function (i, index) {
+        $(".pay_method").hide();
+        $(".pay_cancel").click(function () {
+            $(".pay_method").hide();
+        })
+        /* 判断是否为微信 */
+        /* function isWeiXin(){
+            var ua = window.navigator.userAgent.toLowerCase();
+            if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+                $(".pay_method").find('p').eq(0).hide();
+                return true;
+            }else{
+                $(".pay_method").find('p').eq(1).hide();
+                return false;
+            }
+        } */
+        // isWeiXin();
+
+
+        $(".pay").each(function (i, index) {
             $(index).click(function () {
 
-			
-                if (i == '支付宝') {
-                    var myJson = new Array();
-                    var obj = new Object();
+                if (isWeiXin()) {
 
-                    obj.id = $(index).attr('data-id');
+                    window.location.href = "{{route('payment.gift.wechat_mp')}}";
 
-                    obj.number = 1;
+                } else {
+                    $(".pay_method").show();
+                    $(".pay_submit").on("click", function () {
+                        var pay = $('input:radio[name="pay_method"]:checked').val();
+                        /* alert(pay); */
+                        if (pay == "alipay") {
+                            var myJson = new Array();
+                            var obj = new Object();
 
+                            obj.id = $(index).attr('data-id');
+                            obj.number = 1;
+                            myJson.push(obj)
+                            var strjson = $.toJSON(myJson);
+                            console.log(strjson)
+                            $("input[name='gifts']").val(strjson);
+                            $('.payform').attr('action', '{{route("payment.gift.alipay")}}');
+                            $('.payform').submit();
 
-                    myJson.push(obj)
+                        } else {
+                            var myJson = new Array();
+                            var obj = new Object();
 
+                            obj.id = $(index).attr('data-id');
+                            obj.number = 1;
+                            myJson.push(obj)
+                            var strjson = $.toJSON(myJson);
+                            console.log(strjson)
+                            $("input[name='gifts']").val(strjson);
 
-                    var strjson = $.toJSON(myJson);
-                    console.log(strjson)
-                    $("input[name='gifts']").val(strjson);
-
-                    $('.payform').attr('action', '{{route("payment.gift.alipay")}}');
-                    $('.payform').submit();
+                            $('.payform').attr('action', '{{route("payment.gift.wechat_h5")}}');
+                            $('.payform').submit();
+                        }
+                    });
                 }
-                else {
-                    var myJson = new Array();
-                    var obj = new Object();
-
-                    obj.id = $(index).attr('data-id');
-
-                    obj.number = 1;
 
 
-                    myJson.push(obj)
-
-
-                    var strjson = $.toJSON(myJson);
-                    console.log(strjson)
-                    $("input[name='gifts']").val(strjson);
-
-                    $('.payform').attr('action', '{{route("payment.gift.wechat")}}');
-                    $('.payform').submit();
-                }
-
-            })
-        }) */
+            });
+        });
 
 
         $('.comments').click(function () {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,53 +9,62 @@
     <title>@yield('title', '24小时曝光')</title>
     <!-- 样式 -->
     {{--<link href="{{ mix('css/app.css') }}" rel="stylesheet">--}}
-	<link rel="stylesheet" type="text/css" href="{{asset('web/css/reset.css')}}" />			
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/login.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/personl.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/add.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/exposure-details.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/footer.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/header.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/home.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/my-exposure.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/news.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/newsshow.less')}}">
-	<link rel="stylesheet/less" type="text/css" href="{{asset('web/less/search.less')}}">
-	
-	<link rel="stylesheet" href="{{asset('web/library/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-	
-	<!--<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>-->
-	    <script type="text/javascript" src="{{asset('web/library/jquery/1.9.1/jquery.min.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('web/css/reset.css')}}"/>
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/login.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/personl.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/add.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/exposure-details.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/footer.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/header.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/home.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/my-exposure.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/news.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/newsshow.less')}}">
+    <link rel="stylesheet/less" type="text/css" href="{{asset('web/less/search.less')}}">
 
-	<script type="text/javascript" src="{{asset('web/js/rem.js')}}"></script>
-	<script type="text/javascript" src="{{asset('web/js/less.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('web/library/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+
+    <!--<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>-->
+    <script type="text/javascript" src="{{asset('web/library/jquery/1.9.1/jquery.min.js')}}"></script>
+
+    <script type="text/javascript" src="{{asset('web/js/rem.js')}}"></script>
+    <script type="text/javascript" src="{{asset('web/js/less.min.js')}}"></script>
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
-        @yield('content')
+    @yield('content')
 </div>
 <!-- JS 脚本 -->
 {{--<script src="{{ mix('js/app.js') }}"></script>--}}
 <script>
-	
-		var oHeight = $(document).height(); //浏览器当前的高度
-		$(window).resize(function(){
-			 if($(document).height() < oHeight){
-			 $("footer").css("position","static");
-		 }else{
-			 $("footer").css("position","absolute");
-		 }
-		});
-	
-	
-	/* $(function(){
-		$('input').focus(function(){
-        $('footer').css('display','none');  
+
+    var oHeight = $(document).height(); //浏览器当前的高度
+    $(window).resize(function () {
+        if ($(document).height() < oHeight) {
+            $("footer").css("position", "static");
+        } else {
+            $("footer").css("position", "absolute");
+        }
+    });
+
+    /* 判断是否为微信 */
+    function isWeiXin() {
+        var ua = window.navigator.userAgent.toLowerCase();
+        if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* $(function(){
+        $('input').focus(function(){
+        $('footer').css('display','none');
     })
     $('input').blur(function(){
-        $('footer').css('display','block');  
+        $('footer').css('display','block');
     })
-	}) */
+    }) */
 </script>
 @yield('scriptsAfterJs')
 </body>
